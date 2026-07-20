dafe679c010facd16a512d202aa2dd72059223c0
Logan Girard <logira@meta.com>
2026-07-20 16:25:55 -0700

    refactoring to nix build

--- install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo rm -rf ~/.vim > /dev/null 2>&1
-sudo rm -rf ~/.vimrc > /dev/null 2>&1
-
-custom_dir=.dotfiles
-
-ln -s ~/$custom_dir/.vim ~/.vim
-ln -s ~/$custom_dir/.vimrc ~/.vimrc
